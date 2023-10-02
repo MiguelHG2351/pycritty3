6e33eea2f36123ca156c32a89bcc7164bb7c2df4
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2023-10-01 23:19:39 -0600

    setup setuptools xd

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from pycritty import Pycritty
+from setuptools import setup, find_packages
 
-def main():
-    pycritty = Pycritty()
-
-    pycritty.run()
-
-if __name__ == '__main__':
-    main()
+setup(
+    name='pycritty',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'Toml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pycritty = pycritty.main:main'
+    ]}
+)
